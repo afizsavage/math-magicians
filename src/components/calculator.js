8300c6d95573d17cbdca0eab3429de22c39487d0
Afiz Savage <savageafiz9@gmail.com>
2022-01-12 09:30:57 +0000

    refactor app.js to class based component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,11 +1,12 @@
+/* eslint-disable react/prefer-stateless-function */
+
 import React from 'react';
 
-/*eslint-disable */
 class Calculator extends React.Component {
   render() {
     return (
-        <div className="calc">
-          <div className="cont">
+      <div className="calc">
+        <div className="cont">
           <div>
             <input type="text" value={0} />
           </div>
@@ -13,87 +14,97 @@ class Calculator extends React.Component {
             <tbody>
               <tr>
                 <td>
-                  <button>AC</button>
+                  <button type="button">AC</button>
                 </td>
                 <td>
                   {' '}
-                  <button>+/-</button>
+                  <button type="button">+/-</button>
                 </td>
                 <td>
                   {' '}
-                  <button>%</button>
+                  <button type="button">%</button>
                 </td>
-                <td className="end"><button>+</button></td>
-              </tr>
-              <tr>
-                <td>
-                  {' '}
-                  <button>7</button>
-                </td>
-                <td>
-                  {' '}
-                  <button>8</button>
-                </td>
-                <td>
-                  {' '}
-                  <button>9</button>
-                </td>
-                <td>
-                  {' '}
-                  <button className="end">*</button>
+                <td className="end">
+                  <button type="button">+</button>
                 </td>
               </tr>
               <tr>
                 <td>
                   {' '}
-                  <button>4</button>
+                  <button type="button">7</button>
                 </td>
                 <td>
                   {' '}
-                  <button>5</button>
+                  <button type="button">8</button>
                 </td>
                 <td>
                   {' '}
-                  <button>6</button>
+                  <button type="button">9</button>
                 </td>
                 <td>
                   {' '}
-                  <button className="end">-</button>
+                  <button type="button" className="end">
+                    *
+                  </button>
                 </td>
               </tr>
               <tr>
                 <td>
                   {' '}
-                  <button>1</button>
+                  <button type="button">4</button>
                 </td>
                 <td>
                   {' '}
-                  <button>2</button>
+                  <button type="button">5</button>
                 </td>
                 <td>
                   {' '}
-                  <button>3</button>
+                  <button type="button">6</button>
                 </td>
                 <td>
                   {' '}
-                  <button className="end">+</button>
+                  <button type="button" className="end">
+                    -
+                  </button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  {' '}
+                  <button type="button">1</button>
+                </td>
+                <td>
+                  {' '}
+                  <button type="button">2</button>
+                </td>
+                <td>
+                  {' '}
+                  <button type="button">3</button>
+                </td>
+                <td>
+                  {' '}
+                  <button type="button" className="end">
+                    +
+                  </button>
                 </td>
               </tr>
               <tr>
                 <td colSpan={2}>
                   {' '}
-                  <button>0</button>
+                  <button type="button">0</button>
                 </td>
                 <td>
                   {' '}
-                  <button>.</button>
+                  <button type="button">.</button>
                 </td>
-                <td className="end"><button>=</button></td>
+                <td className="end">
+                  <button type="button">=</button>
+                </td>
               </tr>
             </tbody>
           </table>
-          </div>
         </div>
+      </div>
     );
   }
 }
