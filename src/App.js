45041838baa52080ffbb9af762ad22ebd937eb1a
Afiz Savage <savageafiz9@gmail.com>
2022-01-19 18:07:51 +0000

    Moved routing config from layout to app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 /* eslint-disable react/prefer-stateless-function */
 
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-import Layout from './components/layout';
+import Home from './pages/home';
+import Quote from './pages/quote';
+import Header from './components/header';
+import CalculatorContainer from './pages/calc-cont';
 
 class App extends React.Component {
   render() {
     return (
-      <Layout />
+      <>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/calculator">
+            <CalculatorContainer />
+          </Route>
+          <Route path="/quote">
+            <Quote />
+          </Route>
+        </Switch>
+      </>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,21 +3,16 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-// import App from './App';
+import App from './App';
 import './style.css';
-import Layout from './components/layout';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      {/* <App /> */}
-      <Layout />
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
